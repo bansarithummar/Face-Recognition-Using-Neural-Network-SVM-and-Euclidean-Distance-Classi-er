66eaa349a36d800882bf385c627356b16b0895a6
Bansari Thummar <bansarithummar@gmail.com>
2025-03-08 16:19:48 -0600

    Update Fianlnn.m

--- Fianlnn.m
+++ Fianlnn.m
@@ -1,5 +1,5 @@
 for i = 1 : 75
-    P(:,i) = facedet(['*:\*****\face' int2str(i) '.tiff']);
+    P(:,i) = facedet(['*:\face' int2str(i) '.tiff']);
 end
 P = double(P);
 T = [1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 
@@ -12,7 +12,7 @@ net.trainParam.epochs = 500;
 net.trainParam.goal = 0.001;
 net = train(net,P,T);
 for i = 1 : 25
-    A(:,i)= facedet(['*:\*****\face' int2str(i) '.tiff']);
+    A(:,i)= facedet(['*:\face' int2str(i) '.tiff']);
 end
 Y = Sim(net,A);
 fid = fopen('re1.txt','w+');
